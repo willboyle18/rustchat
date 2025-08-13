1f74700f802017a530a0944f3ed262af9f526e9d
Will Boyle <willboyle18@gmail.com>
2025-08-13 16:07:28 -0700

    Implement /health route and handler function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-use tokio;
+use std::{net::SocketAddr, sync::Arc};
+use tokio::net::{TcpListener, TcpStream};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use tracing::{info};
+use tracing_subscriber;
 
-
-
+#[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let _ = tracing_subscriber::fmt().init();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let app = Router::new().route("/health", get(health));
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    info!("listening on http://{}", addr);
+
+    axum::serve(listener, app)
+        .await
+        .expect("server error");
+}
+
+async fn health() -> impl IntoResponse {
+    (StatusCode::OK, "ok")
 }
