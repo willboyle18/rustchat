bf3547487b88cbc26dd79fcf892707c8db69b0bf
Will Boyle <willboyle18@gmail.com>
2025-08-18 16:08:14 -0700

    Refactor create-tables.sql

---
+++ create-tables.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+DROP TABLE if EXISTS users;
+DROP TABLE if EXISTS messages;
+
+CREATE TABLE users (
+    id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username    TEXT NOT NULL UNIQUE,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE messages (
+    message_id  BIGSERIAL PRIMARY KEY,
+    user_id     BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    text        TEXT NOT NULL CHECK(length(text) <= 4000),
+    sent_at     TIMESTAMPTZ NOT NULL default now()
+);
+
+CREATE INDEX index_messages_user_sent ON messages (user_id, sent_at DESC);
+
+COMMIT;
