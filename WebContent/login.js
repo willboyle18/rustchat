279d1632070edfdd7f5f4ea075b851b8534ea3fa
Will Boyle <willboyle18@gmail.com>
2025-08-25 15:43:49 -0700

    Add DOM element references to login.js

---
+++ WebContent/login.js
@@ -0,0 +1,12 @@
+// Forms
+const loginForm = document.getElementById("login-form");
+const createUserForm = document.getElementById("create-user-form");
+
+// Existing user input boxes
+const passwordInput = document.getElementById("password-input");
+const usernameInput = document.getElementById("username-input");
+
+// New user input boxes
+const newUsernameInput = document.getElementById("new-username-input");
+const newPasswordInput = document.getElementById("new-password-input");
+const confirmPasswordInput = document.getElementById("confirm-password-input");
