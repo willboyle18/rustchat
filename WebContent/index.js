ddf85d5925df0f05d57fd0b0dd92e4bc91447596
Will Boyle <willboyle18@gmail.com>
2025-08-25 15:32:15 -0700

    Change snake case to camelCase in index.js

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,6 +1,6 @@
 const socket = new WebSocket("ws://localhost:3000/ws");
 const form = document.getElementById("chat-form");
-const message_board = document.getElementById("message-board");
+const messageBoard = document.getElementById("message-board");
 const textInput = document.getElementById("chat-input");
 const userInput = document.getElementById("name-input");
 
@@ -24,12 +24,12 @@ form.addEventListener("submit", (e) => {
 socket.onmessage = (event) => {
     const msg = JSON.parse(event.data);
     if (msg.type === "chat") {
-        const new_message = document.createElement("tr");
-        new_message.textContent = msg.text;
-        message_board.appendChild(new_message);
+        const newMessage = document.createElement("tr");
+        newMessage.textContent = msg.text;
+        messageBoard.appendChild(newMessage);
     } else if (msg.type === "system") {
-        const new_message = document.createElement("tr");
-        new_message.textContent = msg.text;
-        message_board.appendChild(new_message);
+        const newMessage = document.createElement("tr");
+        newMessage.textContent = msg.text;
+        messageBoard.appendChild(newMessage);
     }
 };
